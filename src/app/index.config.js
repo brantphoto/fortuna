1401ad19c07e37f9222f040c2bacff02a6191374
brantphoto <brant.barger@gmail.com>
2015-10-29 14:14:48 -0700

    main cards up. To refactor to smaller components

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -5,16 +5,46 @@ export function config ($logProvider,
   // Enable log
   $logProvider.debugEnabled(true);
 
+  $mdThemingProvider.definePalette('gold', {
+    '50': 'ffebee',
+    '100': 'ffcdd2',
+    '200': 'ef9a9a',
+    '300': 'e57373',
+    '400': 'ef5350',
+    '500': 'f5a623',
+    '600': 'e53935',
+    '700': 'd32f2f',
+    '800': 'c62828',
+    '900': 'b71c1c',
+    'A100': 'ff8a80',
+    'A200': 'ff5252',
+    'A400': 'ff1744',
+    'A700': 'd50000',
+    'contrastDefaultColor': 'light'    // whether, by default, text (contrast)
+  });
+  // default theme
   $mdThemingProvider
     .theme('default')
     .primaryPalette('teal', {
      'default': '600',
      'hue-1': 'A200'
     })
-    .accentPalette('pink', {
-      'default': '100'
+    .accentPalette('gold', {
+      'default': '500'
     });
 
-    $mdIconProvider
-      .icon('menu', 'assets/images/menu.svg');
+  // sidebar alt theme
+  $mdThemingProvider.theme('altTheme')
+    .primaryPalette('teal', {
+    'default': '600',
+    'hue-1': 'A200'
+    })
+    .backgroundPalette('teal', {
+      'default': '600'
+    })
+    .dark();
+
+  // icons provided, wrapped by Angular Material system
+  $mdIconProvider
+    .icon('menu', 'assets/images/menu.svg');
 }
